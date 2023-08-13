20abcb7875e37ba7ae5cfda28f81948f0d2ad271
Kyrre Havik <kyrremann@gmail.com>
2023-08-13 22:47:55 +0200

    feat: parse html for data, not just copy the table
    Now I can make more features based on the data.

--- .github/scripts/weather.rb
+++ .github/scripts/weather.rb
@@ -1,19 +1,48 @@
-require "i18n"
+require 'i18n'
+require 'json'
 require 'nokogiri'
 require 'yaml'
 
 I18n.available_locales = [:en]
 
-def parse(place, doc)
-  table = doc.at_css('.table-container')
-  imgs = table.xpath('//img')
-  imgs.each do |img|
-    img['src'] = 'assets/img/' + img['src'].split('/').last.sub('png', 'svg')
+class Row
+  def initialize(tr)
+    @date = tr.children[0].content
+    @notifications = Row.get_img_src(tr.children[1])
+    @weather_symbol_00 = Row.get_img_src(tr.children[2])
+    @weather_symbol_12 = Row.get_img_src(tr.children[3])
+    @temp = tr.children[4].content
+    @rain = tr.children[5].content
+    @wind = tr.children[6].content
   end
 
-  table.traverse { |node| node.remove if node.text? && node.text !~ /\S/ }
+  def self.get_img_src(td)
+    img = td.at('img')
+    return "" unless img
+    { src: img['src'].split('/').last.sub('png', 'svg'), alt: img['alt'] }
+  end
 
-  table
+  def to_json(state)
+    {
+      date: @date,
+      notifications: @notifications,
+      weather_symbol_00: @weather_symbol_00,
+      weather_symbol_12: @weather_symbol_12,
+      temp: @temp,
+      rain: @rain,
+      wind: @wind
+    }.to_json
+  end
+end
+
+def parse_doc(doc)
+  trs = doc.xpath('//tbody//tr')
+  rows = []
+  trs.each do |tr|
+    rows << Row.new(tr)
+  end
+
+  rows
 end
 
 def normalize(name)
@@ -21,14 +50,18 @@ def normalize(name)
 end
 
 places = YAML.load_file('_data/places.yaml')
+weather = []
 
 places.each do |place|
   p "Getting data for #{place['name']}"
-  doc = Nokogiri::HTML5.get("https://www.yr.no/nb/innhold/#{place['id']}/table.html")
-  table = parse(place, doc)
 
-  filename = normalize(place['name'])
-  File.open('_includes/' + filename + '.html', 'w') do |file|
-    file.puts table.to_html(save_with: Nokogiri::XML::Node::SaveOptions::AS_HTML)
+  doc = Nokogiri::HTML5.get("https://www.yr.no/nb/innhold/#{place['id']}/table.html")
+  doc.traverse { |node| node.remove if node.text? && node.text !~ /\S/ }
+
+  dates = parse_doc(doc)
+  weather << { name: place['name'], id: place['id'], dates: dates }
+
+  File.open('_data/weather.json', 'w') do |file|
+    file.puts(weather.to_json)
   end
 end
--- index.md
+++ index.md
@@ -3,19 +3,8 @@ layout: yr
 title: Home
 ---
 
-
-{% assign places = site.data.places | sort: "name" %}
-{% for place in places %}
-<h1 class="location-name">{{ place.name }}</h1>
-{%
-    assign filename = place.name |
-    downcase |
-    replace: "æ", "ae" |
-    replace: "å", "aa" |
-    replace: "ø", "oe" |
-    replace: " ", "-"
-%}
-{% include {{ filename  }}.html %}
+{% for place in site.data.weather %}
+{% include weather_table.html %}
 {% endfor %}
 
 <a target="_blank" href="https://icons8.com/icon/6vvNkta8tr68/alps">alpine</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
