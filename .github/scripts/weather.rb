3a4385a2f4730b676788d588ee9f5b2baf120be5
Kyrre Havik <kyrremann@gmail.com>
2023-08-19 12:18:34 +0200

    feat: better temperature parsing, added temp is warm

--- .github/scripts/weather.rb
+++ .github/scripts/weather.rb
@@ -6,13 +6,34 @@ require 'yaml'
 
 I18n.available_locales = [:en]
 
+class Temp
+  def initialize(tag)
+    self.max(tag[0])
+    self.min(tag[1])
+  end
+
+  def max(tag)
+    @max = tag.content
+    @max_warm = tag.attr('data-is-warm')
+  end
+
+  def min(tag)
+    @min = tag.content
+    @min_warm = tag.attr('data-is-warm')
+  end
+
+  def to_json(state)
+    {max: {temp: @max, is_warm: @max_warm}, min: {temp: @min, is_warm: @min_warm}}.to_json
+  end
+end
+
 class Row
   def initialize(tr)
     @date = tr.children[0].content
-    @notifications = Row.get_img_src(tr.children[1])
+    @notifications = Row.get_img_src(tr.at('.notifications'))
     @weather_symbol_00 = Row.get_img_src(tr.children[2])
     @weather_symbol_12 = Row.get_img_src(tr.children[3])
-    @temp = tr.children[4].content
+    @temp = Temp.new(tr.css('.temperature'))
     @rain = tr.children[5].content
     @wind = tr.children[6].content
   end
