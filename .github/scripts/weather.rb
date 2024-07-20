eb8f671a5b73eb00baa8d0082483c3c31fe07a0e
Kyrre Havik <kyrremann@gmail.com>
2024-07-20 17:51:21 +0200

    feat: ny datakilde, to ekstra dager
    Henter data fra den "vanlige" Yr-siden i stedet for tabellsiden.
    Da får vi to ekstra dager med data.

--- .github/scripts/weather.rb
+++ .github/scripts/weather.rb
@@ -9,18 +9,20 @@ I18n.available_locales = [:en]
 
 class Temp
   def initialize(tag)
-    max(tag[0])
-    min(tag[1])
+    max(tag)
+    min(tag)
   end
 
   def max(tag)
-    @max = tag.content
-    @max_warm = tag.attr('data-is-warm')
+    temp = tag.css('.min-max-temperature__max')
+    @max = temp.children[0].content
+    @max_warm = temp.at_css('.temperature--warm-primary') ? true : false
   end
 
   def min(tag)
-    @min = tag.content
-    @min_warm = tag.attr('data-is-warm')
+    temp = tag.css('.min-max-temperature__min')
+    @min = temp.children[0].content
+    @min_warm = temp.at_css('.temperature--warm-primary') ? true : false
   end
 
   def to_json(_state)
@@ -31,19 +33,21 @@ end
 class Row
   def initialize(tr)
     @date = tr.children[0].content
-    @notifications = Row.get_img_src(tr.at('.notifications'))
-    @weather_symbol_00 = Row.get_img_src(tr.children[2])
-    @weather_symbol_12 = Row.get_img_src(tr.children[3])
-    @temp = Temp.new(tr.css('.temperature'))
-    @rain = tr.children[5].content
-    @wind = tr.children[6].content
+    @notifications = Row.get_img_src(tr.at('warning-icon-image'))
+    weather_img = tr.css('.weather-symbol__img')
+    @weather_symbol_00 = Row.get_img_src(weather_img[0])
+    @weather_symbol_06 = Row.get_img_src(weather_img[1])
+    @weather_symbol_12 = Row.get_img_src(weather_img[2])
+    @weather_symbol_18 = Row.get_img_src(weather_img[3])
+    @temp = Temp.new(tr.css('.daily-weather-list-item__temperature'))
+    @rain = tr.css('.daily-weather-list-item__precipitation').children[0].children[0].children[1].content
+    @wind = tr.css('.wind__value').children[0].content
   end
 
-  def self.get_img_src(td)
-    img = td.at('img')
-    return '' unless img
+  def self.get_img_src(img)
+    return '' if img.nil?
 
-    { src: img['src'].split('/').last.sub('png', 'svg'), alt: img['alt'] }
+    { src: img.attr('src').split('/').last.sub('png', 'svg'), alt: img.attr('alt') }
   end
 
   def to_json(_state)
@@ -51,7 +55,9 @@ class Row
       date: @date,
       notifications: @notifications,
       weather_symbol_00: @weather_symbol_00,
+      weather_symbol_06: @weather_symbol_06,
       weather_symbol_12: @weather_symbol_12,
+      weather_symbol_18: @weather_symbol_18,
       temp: @temp,
       rain: @rain,
       wind: @wind
@@ -79,10 +85,14 @@ weather = []
 places.each do |place|
   p "Getting data for #{place['name']}"
 
-  doc = Nokogiri::HTML5.parse(URI.open("https://www.yr.no/nb/innhold/#{place['id']}/table.html"))
-  doc.traverse { |node| node.remove if node.text? && node.text !~ /\S/ }
+  uri = "https://www.yr.no/nb/v%C3%A6rvarsel/daglig-tabell/#{place['id']}"
+  doc = Nokogiri::HTML5.parse(URI.open(uri))
 
-  dates = parse_doc(doc)
+  dates = []
+  doc.search('.daily-weather-list-item').each do |node|
+    dates << Row.new(node)
+  end
+
   weather << { name: place['name'], id: place['id'], dates: }
 
   File.open('_data/weather.json', 'w') do |file|
